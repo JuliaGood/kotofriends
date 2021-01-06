5a75bdba7e3c8969d0b4010447088680a60884be
JuliaGood <gudak.yuliya@gmail.com>
2021-01-06 20:26:16 +0200

    create Cardlist.js and install tachyons

--- src/index.js
+++ src/index.js
@@ -2,17 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import Card from './Card';
+import CardList from "./CardList";
 import 'tachyons';
 import {robots} from './robots';
 //we must destructure {robots} because that isn't exported by default! That's multiple exports.
 
 ReactDOM.render(
-  <div>
-    <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
-    <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
-    <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
-  </div>
+  <CardList robots={robots}/>
 , document.getElementById('root'));
 
 reportWebVitals();
