d8acda2f1b160742a080df3bb446b03e48515a39
JuliaGood <gudak.yuliya@gmail.com>
2021-01-06 21:12:10 +0200

    remove cardsArray and move it right into the Cardlist component

--- src/CardList.js
+++ src/CardList.js
@@ -2,17 +2,19 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({ robots }) => {
-    const cardsArray = robots.map((robot, i) => {
-        return <Card 
-            key={i} 
-            id={robot.id} 
-            name={robot.name} 
-            email={robot.email}
-            />
-    })
     return ( 
         <div>
-            { cardsArray }
+            { 
+                robots.map((robot, i) => {
+                    return ( <Card 
+                        key={i} 
+                        id={robot.id} 
+                        name={robot.name} 
+                        email={robot.email}
+                        />
+                    )
+                })
+            }
         </div>
     );
 }
