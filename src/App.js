1c9012a143cc858a8e6c7db77d271d75aeea2a24
JuliaGood <gudak.yuliya@gmail.com>
2021-01-08 15:11:55 +0200

    run onSearchChange event

--- src/App.js
+++ src/App.js
@@ -15,18 +15,19 @@ class App extends Component {
     onSearchChange = (event) => {
         console.log(event.target.value);
         this.setState({ searchField: event.target.value });
+    }
+
+    render() { 
         const filteredRobots = this.state.robotsArray.filter(robot => {
             return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
         });
         console.log(filteredRobots);
-    }
 
-    render() { 
         return (
             <div className='tc'> 
                 <h1>robofriends</h1>
                 <SearchBox searchChange={this.onSearchChange} />
-                <CardList robots={this.state.robotsArray}/>
+                <CardList robots={filteredRobots}/>
             </div>
         );
     };
