0ccfcbe86764285608d91567cf630af80811a6eb
JuliaGood <gudak.yuliya@gmail.com>
2021-01-08 14:07:30 +0200

    added App.js and Search.js components

--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import CardList from "./CardList";
+import App from "./App";
 import 'tachyons';
-import {robots} from './robots';
 //we must destructure {robots} because that isn't exported by default! That's multiple exports.
 
 ReactDOM.render(
-  <CardList robots={robots}/>
+  <App />
 , document.getElementById('root'));
 
 reportWebVitals();
